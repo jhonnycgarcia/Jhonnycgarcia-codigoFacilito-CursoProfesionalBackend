5ce9619c24f444da03dfcb07874a3108185999de
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-05-27 17:48:41 -0400

    # Motores de vista

--- html/server.js
+++ html/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use('/public', express.static('./assets'));
 
 app.get('/', function(req, res) {
-    res.sendFile('./index.html', {
-        root: __dirname
-    });
+    res.render('index');
 });
 
 app.listen(3000);
