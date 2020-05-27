e4fc30e88f9a2b8a5a921b821cd55661f26446c8
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-05-27 01:12:06 -0400

    # Primer commit
    Se genera primer servidor de peticiones HTTP.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curso Profesional de BackEnd (CodigoFacilito)
+
+Este curso tiene como finalidad aprender lso fundamentos basicos para el manejo y desarrollo de aplicaciones WEB con una arquitectura REST.
---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+function responderPeticion(request, response) {
+    response.end('Hola mundo!');
+}
+
+let server = http.createServer(responderPeticion);
+server.listen(3000);
