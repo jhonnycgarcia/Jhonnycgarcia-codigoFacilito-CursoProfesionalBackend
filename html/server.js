f74b2ac7051b1183d1de6d188ee3a42502becf6c
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-05-27 17:41:46 -0400

    # Servir archivos estaticos

--- html/server.js
+++ html/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.use('/public', express.static('./assets'));
 
 app.get('/', function(req, res) {
     res.sendFile('./index.html', {
