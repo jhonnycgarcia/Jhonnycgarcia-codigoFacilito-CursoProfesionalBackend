e9d6d2421693cdc64e35b82b6d8bcd69bfcd38ae
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-05-28 00:56:51 -0400

    # Configurar SquelizeJs

--- base_datos/server.js
+++ base_datos/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let db = new sqlite3.Database('proyecto_backend'); // Establecer conexion con la DB
+// let db = new sqlite3.Database('proyecto_backend'); // Establecer conexion con la DB
+const sequelize = new Sequelize('proyecto_backend', null, null, {
+    dialect: 'sqlite', // motor de DB
+    storage: './proyecto_backend' // ubicacion del archivo
+});
 
 // db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255) )'); // Crear la tabla
 
 app.post('/pendientes', function(req, res) {
-    db.run("INSERT INTO tasks(description) VALUES(?)", req.body.description);
+    // db.run("INSERT INTO tasks(description) VALUES(?)", req.body.description);
     res.send('Insercion finalizada');
 });
 
@@ -19,6 +24,5 @@ app.listen(3000);
 
 process.on('SIGINT', function() { // evento de presionar Ctrl + C
     console.log('Server was disconnected!');
-    db.close(); // Cerrar conexion con la DB
     process.exit(); // Elimina el servidor
 });
