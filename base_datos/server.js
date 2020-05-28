2d965abd1da8c7aac9c390cf92407058d65a5cb1
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-05-27 23:53:02 -0400

    # Crear una tabla (primera consulta)

---
+++ base_datos/server.js
@@ -0,0 +1,7 @@
+const sqlite3 = require('sqlite3');
+
+let db = new sqlite3.Database('proyecto_backend');
+
+db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255) )');
+
+db.close();
