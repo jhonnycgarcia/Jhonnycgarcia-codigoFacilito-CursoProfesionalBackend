0be224a6d643332d9e419cbe05605581136abe50
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-05-28 00:21:02 -0400

    # Insertar registros en la base de datos

--- base_datos/server.js
+++ base_datos/server.js
@@ -1,7 +1,24 @@
+const express = require('express');
 const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 
-let db = new sqlite3.Database('proyecto_backend');
+const app = express();
 
-db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255) )');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-db.close();
+let db = new sqlite3.Database('proyecto_backend'); // Establecer conexion con la DB
+
+// db.run('CREATE TABLE tasks(id int AUTO_INCREMENT, description varchar(255) )'); // Crear la tabla
+
+app.post('/pendientes', function(req, res) {
+    db.run("INSERT INTO tasks(description) VALUES('Hola Mundo!')");
+    res.send('Insercion finalizada');
+});
+
+app.listen(3000);
+
+process.on('SIGINT', function() { // evento de presionar Ctrl + C
+    console.log('Server was disconnected!');
+    db.close(); // Cerrar conexion con la DB
+    process.exit(); // Elimina el servidor
+});
