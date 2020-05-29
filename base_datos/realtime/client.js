0b580da0ecdf70e1b5b53c9c2a8cf78742ab27df
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-05-28 23:29:26 -0400

    # Corregir host en los websockets

--- base_datos/realtime/client.js
+++ base_datos/realtime/client.js
@@ -1,6 +1,12 @@
 const io = require('socket.io-client');
 
-let socket = io.connect('http://localhost:3000', { reconnect: true });
+let host = 'http://localhost:3000';
+if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') { // si se encuentra en entorno de producion
+    // cambiar host
+    host = 'https://blooming-inlet-55842.herokuapp.com'
+}
+
+let socket = io.connect(host, { reconnect: true });
 
 socket.on('connect', function() {
     console.log("\n\nSocket connected from NodeJs\n\n");
